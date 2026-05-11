51cf24eb19bdfbca73ee3e50da4eca3064e7af7e
ankush <raneankush93@gmail.com>
2026-05-11 21:41:57 +0530

    feat: password hashing added

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,13 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     DATABASE_URL : str
+
     GEMINI_API_KEY : str
     GROQ_API_KEY : str
+    
+    SECRET_KEY : str
+    ALGORITHM : str
+    ACCESS_TOKEN_EXPIRE_MINUTES : int
 
 
     class Config:
---
+++ app/security/jwt_handler.py
@@ -0,0 +1,4 @@
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from app.core.config import settings
+
