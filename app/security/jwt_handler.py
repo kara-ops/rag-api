db335c9df830780ca94bd2d0a399a926f57abe4d
ankush <raneankush93@gmail.com>
2026-05-12 23:04:42 +0530

    feat: decode and encode access token

--- app/security/jwt_handler.py
+++ app/security/jwt_handler.py
@@ -1,4 +1,18 @@
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
 from app.core.config import settings
+from fastapi import HTTPException
 
+
+def create_access_token(data:dict)->str:
+    to_encode = data.copy()
+    expiry = datetime.now() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp":expiry})
+    return jwt.encode(to_encode,settings.SECRET_KEY,settings.ALGORITHM)
+
+def decode_access_token(token:str)->dict:
+    try:
+        payload = jwt.decode(token,settings.SECRET_KEY,settings.ALGORITHM)
+    except JWTError:
+        return None
+    return payload
